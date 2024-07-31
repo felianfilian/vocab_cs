97a554897aa4437216f1d4b782c7198f976bcee5
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-01 01:03:33 +0200

    first tests

--- vocab_cs/Program.cs
+++ vocab_cs/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration.Assemblies;
+
+internal class Program
+{
+    
+
+    private static void Main(string[] args)
+    {
+        const float pi = 3.1234f;
+        string vocab = "hello";
+        Console.WriteLine(vocab + " world of " + pi);
+    }
+}
